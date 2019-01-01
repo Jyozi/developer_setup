c2638fd4e9b9155d91d9c6532ef012639fc89aee
Jyozi <appmacsk.by.icloud@gmail.com>
2019-01-01 22:18:05 +0900

    2019/1/1

--- make_develop.sh
+++ make_develop.sh
@@ -13,7 +13,7 @@ if [ -z `uname | grep "Drawin"` ]; then
     #sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS*.pkg -target /
     pyenv install 3.6.1
     pyenv global 3.6.1
-    pip install pipenv neovim isort jedi autopep8
+    pip install pipenv neovim isort jedi autopep8 flake8
     pip install --upgrade pip
 elif [ -z `which yum` ]; then
     # For Fedora CentOS
@@ -22,7 +22,7 @@ elif [ -z `which yum` ]; then
                     sqlite-devel openssl-devel xz xz-devel libffi-devel
     pyenv install 3.6.1
     pyenv global 3.6.1
-    pip install pipenv neovim isort jedi autopep8
+    pip install pipenv neovim isort jedi autopep8 flake8
     pip install --upgrade pip
 elif [ -z `which apt-get` ];then
     # For Ubuntu
@@ -33,7 +33,7 @@ elif [ -z `which apt-get` ];then
                             libffi-dev liblzma-dev python-openssl
     pyenv install 3.6.1
     pyenv global 3.6.1
-    pip install pipenv neovim isort jedi autopep8
+    pip install pipenv neovim isort jedi autopep8 flake8
     pip install --upgrade pip
 fi
 
