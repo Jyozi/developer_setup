0d74572364941db79d5d1f7756f95e0052580ffe
Jyozi <appmacsk.by.icloud@gmail.com>
2020-02-02 20:27:41 +0900

    nvim config

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# developer_setup
-## How to
-```
-sh make_setup.sh
-```
-
-## Detail
-```
-Neovim
-Flake8
-autopep8
-vim-jedi
-supertab
-NeardTree
-```
+# neovim config
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+#set -e
+
+cp -r nvim ~/.config/nvim
+
+# brew install neovim pyenv readline xz
+# pyenv install 3.6.1
+# pyenv global 3.6.1
+# pip install pipenv neovim isort jedi autopep8 flake8
+# pip install --upgrade pip
+# 
+# yum install -y neovim pyenv
+# yum install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite \
+#                 sqlite-devel openssl-devel xz xz-devel libffi-devel
+# pyenv install 3.6.1
+# pyenv global 3.6.1
+# pip install pipenv neovim isort jedi autopep8 flake8
+# pip install --upgrade pip
+# 
+# sudo apt-get install -y neovim pyenv
+# sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+#                         libreadline-dev libsqlite3-dev wget curl llvm \
+#                         libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+#                         libffi-dev liblzma-dev python-openssl
+# pyenv install 3.6.1
+# pyenv global 3.6.1
+# pip install pipenv neovim isort jedi autopep8 flake8
+# pip install --upgrade pip
